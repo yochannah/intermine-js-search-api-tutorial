fd43a343b1ef59f93f42461eb46ab380d64b3313
Yo Yehudi <yochannah@gmail.com>
2017-03-15 14:27:43 +0000

    add a bunch of documentation

--- scripts.js
+++ scripts.js
@@ -88,26 +88,39 @@ searchForm.addEventListener("submit", function(event){
     resultsBox.innerHTML = "";
 
     //go over the fields for the results and output them to the screen
-    response.results.map(function(singleResult) {
+    for (key in response.results) {
+      var singleResult = response.results[key];
       //here we're adding one div row per result, and highlighting the result type in bold.
       resultsBox.innerHTML += "<div class='result'>"
+      //output the result type (e.g. Gene, Protein, publication)
       + "<strong>" + singleResult.type + ": </strong>"
       //output all the results in the "fields" section of the json:
-    //  + JSON.stringify(singleResult.fields)
       + fieldsList(singleResult.fields)
       + "</div>"
-    });
+    };
   });
 });
 
+//This function outputs an HTML list, given a javascript object containing keys and values
+// so the input {"symbol": "PPARG", "organism.name": "Homo sapiens"} will become
+// <ul>
+//   <li><strong>symbol: </strong> PPARG</li>
+//   <li><strong>organism.name: </strong> Homo sapiens</li>
+// </ul>
 function fieldsList(fields){
-  console.log('fields', fields);
+  //create a variable to store the html in, and open a list tag.
+  //<ul> stands for 'unordered list'
   var fieldsHTML = "<ul>";
-  for (var key in fields) {
-    console.log (key, fields[key]);
-    var value = fields[key];
-    fieldsHTML = fieldsHTML + "<li> "
-      + "<strong>" + key + ":</strong> " + value + "</li>";
-  };
+  //loop over the values in the javascript object
+    for (var key in fields) {
+      //access the value inside the object and store in in a variable
+      var value = fields[key];
+      //add the key and value to the HTML we'll be returning
+      //<li> stands for list item
+      fieldsHTML = fieldsHTML + "<li> "
+        // <strong> makes things bold by default
+        + "<strong>" + key + ":</strong> " + value + "</li>";
+    };
+    //close the unordered list tag and return the value to whatever function called it.
   return fieldsHTML + "</ul>";
 }
